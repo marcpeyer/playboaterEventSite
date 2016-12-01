e0c6db188705a75a85cbace5da75242d901749e0
Marc Peyer <marc.peyer@gmail.com>
2016-12-01 23:49:03 +0100

    Initial netFx4.6.1 project

---
+++ EventSiteWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventSiteWeb.Startup))]
+namespace EventSiteWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
